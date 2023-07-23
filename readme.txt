1bd737a7fb8e2b71939d6189b378ee13d24bc430
Matthew Rada <matthewr7.christopher@gmail.com>
2023-07-23 13:55:15 -0400

    yay

---
+++ readme.txt
@@ -0,0 +1,5 @@
+swiper.js demos - https://swiperjs.com/demos#default
+swiper.js docs - https://swiperjs.com/get-started
+
+yellow - rgb: 240, 191, 76, hex: #F0BF4C
+blue - rgb: 8, 77, 162, hex: #084DA2
---
+++ slide-functions.js
@@ -0,0 +1,18 @@
+var years = [
+2003, 2004, 2005, 2006, 2007, 
+2008, 2009, 2010, 2011, 2012, 
+2013, 2014, 2015, 2016, 2017, 
+2018, 2019, 2020, 2021, 2022, 2023]
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "fade",
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (years[index]) + '</span>';
+      },
+    dynamicBullets: true,
+  },
+  keyboard: true,
+});
